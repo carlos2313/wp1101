dcdad08f6b5701ce26a0e46e880d0a8e0865a8a8
Carlos Chen <carlos.chen.yaya@gmail.com>
2021-12-07 11:38:26 +0800

    hope not wrong

---
+++ hack2/server/routes/post.js
@@ -0,0 +1,82 @@
+import express from 'express'
+import Post from '../models/post'
+import moment from 'moment'
+
+const router = express.Router()
+
+// TODO 2-(1): create the 1st API (/api/allPosts)
+router.get('/allPosts', async (_, res) => {
+    var Posts;
+    var responseMessage = '';
+    try{
+        Posts = await Post.find().sort( { timestamp: -1 } );
+        responseMessage = "success";
+        // console.log(Posts)
+        if(Posts.length === 0){
+            Posts = null;
+            responseMessage = "error";
+            res.status(403).json({ data: Posts, message: responseMessage});
+        }
+    }catch(e){
+        Posts = null;
+        responseMessage = "error";
+        res.status(403).json({ data: Posts, message: responseMessage});
+        throw new Error("Database get allpost failed"); 
+    }
+    res.json({ data: Posts, message: responseMessage});
+});
+// TODO 3-(1): create the 2nd API (/api/postDetail)
+router.get('/postDetail', async (req, res) => {
+    var aPost;
+    var responseMessage = '';
+    try{
+        aPost = await Post.find({postId: req.query.pid});
+        responseMessage = "success";
+        console.log(aPost)
+        if(aPost.length === 0){
+            aPost = null;
+            responseMessage = "error";
+            res.status(403).json({ post: aPost, message: responseMessage});
+        }
+    }catch(e){
+        aPost = null;
+        responseMessage = "error";
+        res.status(403).json({ post: aPost, message: responseMessage});
+        throw new Error("Database get allpost failed"); 
+    }
+    res.json({ post: aPost[0], message: responseMessage});
+});
+// TODO 4-(1): create the 3rd API (/api/newPost)
+router.post('/newPost', async (req, res) => {
+    console.log(req.body)
+    var aPost;
+    var responseMessage = '';
+    try{
+        aPost = await Post.create({postId: req.body.postId, title: req.body.title, content: req.body.content, timestamp: req.body.timestamp});
+        responseMessage = "success";
+        console.log(aPost);
+    }catch(e){
+        responseMessage = "error";
+        res.status(403).json({message: responseMessage});
+        throw new Error("Database get allpost failed"); 
+    }
+    res.json({message: responseMessage});
+});
+// TODO 5-(1): create the 4th API (/api/post)
+router.delete('/post', async (req, res) => {
+    console.log('why')
+    console.log(req.body)
+    var aPost;
+    var responseMessage = '';
+    try{
+        aPost = await Post.deleteOne({postId: req.query.pid});
+        responseMessage = "success";
+        console.log(aPost);
+    }catch(e){
+        responseMessage = "error";
+        res.status(403).json({message: responseMessage, post: null});
+        throw new Error("Database get allpost failed"); 
+    }
+    res.json({message: responseMessage});
+});
+export default router
---
+++ hack2/src/edit.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import instance from './instance'
+
+import { Button, TextField } from '@material-ui/core'
+import { Delete as DeleteIcon, Send as SendIcon } from '@material-ui/icons'
+import { v4 as uuidv4 } from 'uuid'
+
+function Edit(props) {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const handleChange = (func) => (event) => {
+    func(event.target.value);
+  };
+  
+  // TODO 4-(2): complete handleSubmit function to create a new post and save it to database 
+  const handleSubmit = async () => {
+    setTitle(title.trim());
+    setContent(content.trim());
+    const postId = uuidv4();
+    const timestamp = new Date();
+    if(title!=='' && content!==''){
+      const {
+        data: { message },
+    } = await instance.post('/newPost', {postId, title, content, timestamp});
+      console.log(message);
+    }
+    setTimeout(() => {
+      props.navigate(-1);
+    }, 300)
+  }
+
+  return (
+    <div className="post-wrapper">
+      <div className="post-text-container">
+        <div style={{ fontWeight: 'Bold', fontSize: 18 }}>Create a New Post</div>
+
+        <div className="post-title">
+          {/* TODO 4-(2): add property to Textfield to store the input */}
+          <TextField
+            label="Title"
+            size="small"
+            variant="outlined"
+            className="post-title"
+            id="pid-create-title"
+            onChange={handleChange(setTitle)}
+          />
+        </div>
+
+        <div className="post-content-container">
+          {/* TODO 4-(2): add property to Textfield to store the input */}
+          <TextField
+            label="Content"
+            variant="outlined"
+            className="post-content-editor"
+            id="pid-create-content"
+            multiline
+            onChange={handleChange(setContent)}
+          />
+        </div>
+
+        <div className="post-btn-wrapper">
+          <Button variant="contained" color="primary" className="post-btn" startIcon={<SendIcon />} id="pid-create-submit-btn" onClick={handleSubmit}>Submit</Button>
+          <Button variant="contained" color="secondary" className="post-cancel-btn" endIcon={<DeleteIcon />} onClick={e => props.navigate(-1)}>Cancel</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Edit
